a6d97db608fc28be4c34dab4f9a0902237a4ef12
Raz Luvaton <rluvaton@gmail.com>
2019-12-12 15:04:28 +0200

    Update GetAllStudents.js
    Clean the code

--- src/application/use_cases/GetAllStudents.js
+++ src/application/use_cases/GetAllStudents.js
@@ -3,10 +3,7 @@ const Student = require('../../entities/Student');
 module.exports = (StudentRepository) => {
 
     async function Execute(firstName, lastName, email) {
-        const students = await StudentRepository.getAll();
-        return new Promise((resolve, reject) => {
-            resolve(students);
-        });
+        return StudentRepository.getAll();
     }
 
     return {
