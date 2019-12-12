e6a734cd5d47dd17a2d83c83b7a90446b0e7c3d1
Raz Luvaton <rluvaton@gmail.com>
2019-12-12 14:43:54 +0200

    Clean AddEntrollment.js file
    Remove unnecessary code

--- src/application/use_cases/AddEnrollment.js
+++ src/application/use_cases/AddEnrollment.js
@@ -3,27 +3,21 @@ module.exports = (StudentRepository) => {
 
     const Execute = async (studentId, enrollment) => {
         const student = await StudentRepository.getById(studentId);
-        return new Promise((resolve, reject) => {
-            // business validation
-            if (!student || !enrollment) {
-                reject(new Error('validation failed'));
-                return;
-            }
-            // to be implemented - check if course exist
-            // check if user already has this enrollment
-            if (student.enrollments.some(e => e.course.id === enrollment.course.id)) {
-                reject(new Error('validation failed: user already registered to this course'));
-                return;
-            }
 
-            // can add validation with joi
+        // business validation
+        if (!student || !enrollment) {
+            throw new Error('validation failed');
+        }
 
-            StudentRepository.addEnrollment(studentId, enrollment).then((response) => {
-                resolve(response);
-            }, (err) => {
-                reject(err);
-            });
-        });
+        // to be implemented - check if course exist
+        // check if user already has this enrollment
+        if (student.enrollments.some(e => e.course.id === enrollment.course.id)) {
+            throw new Error('validation failed: user already registered to this course');
+        }
+
+        // can add validation with joi
+
+        return StudentRepository.addEnrollment(studentId, enrollment);
     };
     return {
         Execute
