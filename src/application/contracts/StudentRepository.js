034241218b75b9c4d23e7a21d99e1d1abd5bbbe7
Raz Luvaton <rluvaton@gmail.com>
2019-12-12 16:14:39 +0200

    Update StudentRepository.js
    Remove unnecessary code

--- src/application/contracts/StudentRepository.js
+++ src/application/contracts/StudentRepository.js
@@ -5,44 +5,30 @@ module.exports = class StudentRepository {
     constructor() { }
 
     add(studentInstance) {
-        return new Promise((resolve, reject) => {
-            reject(new Error('not implemented'));
-        });
+        return Promise.reject(new Error('not implemented'));
     }
 
     update(studentInstance) {
-        return new Promise((resolve, reject) => {
-            reject(new Error('not implemented'));
-        });
+        return Promise.reject(new Error('not implemented'));
     }
 
     delete(studentInstance) {
-        return new Promise((resolve, reject) => {
-            reject(new Error('not implemented'));
-        });
+        return Promise.reject(new Error('not implemented'));
     }
 
     getById(StudentId) {
-        return new Promise((resolve, reject) => {
-            reject(new Error('not implemented'));
-        });
+        return Promise.reject(new Error('not implemented'));
     }
 
     getByEmail(StudentId) {
-        return new Promise((resolve, reject) => {
-            reject(new Error('not implemented'));
-        });
+        return Promise.reject(new Error('not implemented'));
     }
 
     getAll() {
-        return new Promise((resolve, reject) => {
-            reject(new Error('not implemented'));
-        });
+        return Promise.reject(new Error('not implemented'));
     }
 
     addEnrollment(studentInstance, enrollment) {
-        return new Promise((resolve, reject) => {
-            reject(new Error('not implemented'));
-        });
+        return Promise.reject(new Error('not implemented'));
     }
 };
